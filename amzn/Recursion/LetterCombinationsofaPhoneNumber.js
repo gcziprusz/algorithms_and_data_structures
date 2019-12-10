f224774a9e8daf63d7b0f87d2ab39cfb9dfb9f3b
Gabriel Cziprusz <gcziprusz@gmail.com>
2019-12-10 07:43:10 -0600

    Update LetterCombinationsofaPhoneNumber.js

--- amzn/Recursion/LetterCombinationsofaPhoneNumber.js
+++ amzn/Recursion/LetterCombinationsofaPhoneNumber.js
@@ -1,43 +1,45 @@
-  var phone = {
-    "2": "abc",
-    "3": "def",
-    "4": "ghi",
-    "5": "jkl",
-    "6": "mno",
-    "7": "pqrs",
-    "8": "tuv",
-    "9": "wxyz"
-  };
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var phone = {
+"2": "abc",
+"3": "def",
+"4": "ghi",
+"5": "jkl",
+"6": "mno",
+"7": "pqrs",
+"8": "tuv",
+"9": "wxyz"
+};
 
-var output = [];
 
-function backtrack( combination, next_digits) {
-  // if there is no more digits to check
-  if (next_digits.length === 0) {
-    // the combination is done
-    output.push(combination);
-  }
-  // if there are still digits to check
-  else {
-    // iterate over all letters which map 
-    // the next available digit
-    var digit = next_digits.substring(0, 1);
-    var letters = phone[digit];
-    for (var i = 0; i < letters.length; i++) {
-      var letter = phone[digit].substring(i, i + 1);
-      // append the current letter to the combination
-      // and proceed to the next digits
-      backtrack(combination + letter, next_digits.substring(1));
+
+var backtrack = function(output, accumulator, nextDigit) {
+    if(nextDigit.length === 0){
+        output.push(accumulator);
     }
-  }
+    else{
+        // get next digit
+        var digit = nextDigit.substring(0,1);
+        // get letters associated with current digit
+        var letters = phone[digit];
+        // loop over letters 
+        for(var i=0;i < letters.length;i++){
+            var letter = letters.substring(i,i+1);
+            backtrack(output,accumulator+letter, nextDigit.substring(1));
+        }
+            
+        // reecurse with added letter to accumulator + kovetkezo digit
+    }
 }
 
-function letterCombinations(digits) {
-  if (digits.length !== 0)
-    backtrack("", digits);
-  return output;
-}
 
-console.log("letterCombinations",JSON.stringify(letterCombinations("23")) === JSON.stringify(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]));
-
-
+var letterCombinations = function(digits) {
+    var output = [];
+    if(digits.length !== 0) {
+        backtrack(output, "",digits);
+    }
+    return output;
+        
+};
