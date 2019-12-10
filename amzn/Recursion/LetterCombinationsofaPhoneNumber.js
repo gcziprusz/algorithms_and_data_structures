22ed4c0fa7909efd6df22c1824f3625de050818a
Gabriel Cziprusz <gcziprusz@gmail.com>
2019-12-10 07:44:25 -0600

    Update LetterCombinationsofaPhoneNumber.js

--- amzn/Recursion/LetterCombinationsofaPhoneNumber.js
+++ amzn/Recursion/LetterCombinationsofaPhoneNumber.js
@@ -43,3 +43,47 @@ var letterCombinations = function(digits) {
     return output;
         
 };
+
+
+
+
+/**
+ * FASTER SOLUTION
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    
+    if (!digits) return [];
+    
+    const map = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
+    };
+    
+    const answer = [];
+    
+    const recurse = str => {
+        
+        if (str.length === digits.length) {
+            answer.push(str);
+            return;
+        }
+        
+        let chars = map[digits[str.length]];
+        
+        for (let i = 0; i < chars.length; i++) {
+            recurse(str + chars[i]);
+        }
+    }
+    
+    recurse('');
+    
+    return answer;
+};
