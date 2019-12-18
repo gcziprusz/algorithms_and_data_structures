6c920eec22151299ee42a2c08d0ea001c0462fef
Gabriel Cziprusz <gcziprusz@gmail.com>
2019-12-18 05:00:11 -0600

    Update RotateImage.js

--- amzn/Arrays and Strings/RotateImage.js
+++ amzn/Arrays and Strings/RotateImage.js
@@ -1,3 +1,34 @@
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+// function rotate(matrix) {
+//      matrix.reverse();
+//     for (var i = 0; i < matrix.length; ++i) {
+//         for (var j = i + 1; j < matrix[i].length; ++j)
+//             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+//     }
+// }
+
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+// function anti_rotate(matrix) {
+//     for (auto vi : matrix) reverse(vi.begin(), vi.end());
+//     for (int i = 0; i < matrix.size(); ++i) {
+//         for (int j = i + 1; j < matrix[i].size(); ++j)
+//             swap(matrix[i][j], matrix[j][i]);
+//     }
+// }
+
+
 // Here is the input
 
 // let matrix = 
