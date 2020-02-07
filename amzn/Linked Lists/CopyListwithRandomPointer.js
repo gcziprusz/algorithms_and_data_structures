d6fe4753473d835ffe3ad14cd641ad75f2a07c71
Gabriel Cziprusz <gcziprusz@gmail.com>
2020-02-07 08:21:55 -0600

    Update CopyListwithRandomPointer.js

--- amzn/Linked Lists/CopyListwithRandomPointer.js
+++ amzn/Linked Lists/CopyListwithRandomPointer.js
@@ -1,4 +1,4 @@
-/**
+/*************** RECURSIVE ****************
  * // Definition for a Node.
  * function Node(val, next, random) {
  *    this.val = val;
@@ -21,4 +21,47 @@
         nodeCopy.next = copyRandomList(head.next);
         nodeCopy.random = copyRandomList(head.random);
         return nodeCopy;    
+};
+
+
+/***************** ITERATIVE ****************
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+let visitedNodeDict = new Map();
+
+function getOrCreateNode(node){
+    if(!node){
+        return null;
+    }
+    
+    if (!visitedNodeDict.has(node)){
+        visitedNodeDict.set(node, new Node(node.val))
+    }
+    return visitedNodeDict.get(node)
+}
+    
+var copyRandomList = function(head) {
+    if (!head) return null;
+
+    var newHead = new Node(head.val);
+    var oldHead = head
+    visitedNodeDict.set(head, newHead);
+
+    while(oldHead){
+        newHead.next =  getOrCreateNode(oldHead.next); 
+        newHead.random =  getOrCreateNode(oldHead.random);
+
+        oldHead = oldHead.next;
+        newHead = newHead.next;
+    }
+    return visitedNodeDict.get(head)
 };
