aebdd19146f9b9c5733822a30a3095d5372d33eb
Gabriel Cziprusz <gcziprusz@gmail.com>
2020-02-19 07:12:28 -0600

    Update BinaryTreeLevelOrderTraversal.js

--- amzn/Trees and Graphs/BinaryTreeLevelOrderTraversal.js
+++ amzn/Trees and Graphs/BinaryTreeLevelOrderTraversal.js
@@ -23,3 +23,40 @@ var levelOrder = function(root) {
     }
     return levels;
 };
+
+/** ITERATIVE
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    let levels = [];
+    if (root === null) return levels;
+    
+    let queue = [];
+    let level = 0;
+    
+    queue.push(root);
+    
+    while(queue.length !== 0){
+        levels.push([]);
+        let len = queue.length;
+        for(var i =0; i< len; i++){
+            let n = queue.shift();      
+         
+            levels[level].push(n.val);
+        
+            if(n.left !== null) queue.push(n.left);
+            if(n.right !== null) queue.push(n.right);    
+        }
+        
+        level++;
+    }
+    return levels;
+};
