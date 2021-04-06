67f6fa62f4d3f29fd0df13cf852f2b1af025f0ee
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-04-05 20:59:04 -0500

    iterative sol

--- amzn/Linked Lists/MergeTwoSortedLists.js
+++ amzn/Linked Lists/MergeTwoSortedLists.js
@@ -29,3 +29,44 @@ function mergeTwoLists( l1,  l2) {
      return l2;
   }
 }
+
+
+/* Iterative solution */
+/**
+ * https://www.youtube.com/watch?v=s8kQm8yhZ8U
+ *
+ *
+ *
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+    
+
+
+
+function mergeTwoLists( l1,  l2) {
+ var res = new ListNode(-1);
+ var head = res;
+ while(l1!==null&&l2!==null){
+     if(l1.val <= l2.val){
+         res.next = l1;
+         l1 = l1.next;
+     } else {
+         res.next = l2;
+         l2 =l2.next;
+     }
+     res = res.next;
+}
+    res.next = l1===null? l2:l1;
+ return head.next
+}
