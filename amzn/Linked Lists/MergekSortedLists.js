9075677827ba8e37c8de0acfd7bbd4d2318f610d
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-04-07 06:58:28 -0500

    Update MergekSortedLists.js

--- amzn/Linked Lists/MergekSortedLists.js
+++ amzn/Linked Lists/MergekSortedLists.js
@@ -43,3 +43,24 @@ function mergeTwoLists(a,b){
     }
     return tmpHead.next;
 }
+
+/***********RECURSIVE*************/
+var mergeKLists = function(lists) {
+    let merged = null;
+    for(list of lists){
+        merged = merge(merged,list);
+    }
+    return merged;
+};
+
+var merge = function(l1, l2){
+     if (l2 && l1) {
+        if (l1.val < l2.val) {
+          l1.next = merge(l1.next, l2);
+          return l1;
+        }
+        l2.next = merge(l2.next, l1);
+        return l2;
+    }
+    return l1 || l2;
+}
