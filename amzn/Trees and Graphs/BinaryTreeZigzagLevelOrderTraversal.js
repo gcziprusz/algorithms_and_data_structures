6322ad529597670d340c9c852edd537c0e7585f5
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-04-09 16:45:57 -0500

    Update BinaryTreeZigzagLevelOrderTraversal.js

--- amzn/Trees and Graphs/BinaryTreeZigzagLevelOrderTraversal.js
+++ amzn/Trees and Graphs/BinaryTreeZigzagLevelOrderTraversal.js
@@ -79,4 +79,34 @@ var zigzagLevelOrder = function(root) {
     zig(root,0)
     return result;
 };
+/*****ALTERNATIVE DFS SOLUTION******/
+var zigzagLevelOrder = function(root) {
+    function dfs(level,direction,root) {
+        if(root) {
+        
+            if(level >= result.length) {
+                result.push([]);
+            }
+
+            if(direction === 0) {
+                result[level].push(root.val);
+            }
+
+            if(direction === 1) {
+                result[level].unshift(root.val);
+            }
+
+            if(root.left) {
+                dfs(level+1, (direction+1)%2, root.left);
+            }
+
+            if(root.right) {
+                dfs(level+1, (direction+1)%2, root.right);
+            }
+        }
+    }
+    var result = [];
+    dfs(0,0,root);
+    return result;
+}; 
 
