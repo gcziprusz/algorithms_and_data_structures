f7f8e5f864112835a4b89bc3c0cb8d92f4e84cd3
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-04-15 07:17:23 -0500

    Update WordLadder2.js

--- amzn/Trees and Graphs/WordLadder2.js
+++ amzn/Trees and Graphs/WordLadder2.js
@@ -1,32 +1,35 @@
+
+  
 var findLadders=function(beginWord, endWord, wordList){
     let results=[]
-    let visited={}
+    let visited= new Map();
     let steps=Number.MAX_SAFE_INTEGER
     let pathq=[[beginWord]]
-    let wordset=new Set(wordList)
+    let wordset=new Set(wordList);
+    let len = beginWord.length;
     while(pathq.length>0){
-        let curpath=pathq.shift()
+        let curpath=pathq.shift();
         let curword=curpath[curpath.length-1]
         if(curpath.length>=steps){
                 break
         }
         for(let i = 97; i < 123; i++){
-            for(let j=0;j<curword.length;j++){
-            let w=curword.substring(0, j)+String.fromCharCode(i)+curword.substring(j+1)
-            if(w!==curword&&wordset.has(w)){
-               if(!visited[w]){
-                    pathq.push([...curpath,w])
-                }
-                if(w===endWord){
-                    if(curpath.length<steps){
-                        steps=curpath.length+1
+            for(let j=0;j<len;j++){
+                let w=curword.substring(0, j)+String.fromCharCode(i)+curword.substring(j+1)
+                if(w!==curword&&wordset.has(w)){
+                    if(!visited.has(w)){
+                        pathq.push([...curpath,w])
                     }
-                    results.push([...curpath,w])
+                    if(w===endWord){
+                        if(curpath.length<steps){
+                         steps=curpath.length+1
+                        }
+                        results.push([...curpath,w])
+                    }
                 }
             }
         }
-    }
-        visited[curword]=true
+        visited.set(curword,true);
     }
     return results
 }
