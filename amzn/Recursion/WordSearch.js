3dba93ff5d4e2f22467ec5f7c0671e3aed9d3292
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-04-22 18:59:05 -0500

    Update WordSearch.js

--- amzn/Recursion/WordSearch.js
+++ amzn/Recursion/WordSearch.js
@@ -4,8 +4,10 @@
 
     for (let row = 0; row < ROWS; ++row)
       for (let col = 0; col < COLS; ++col)
-        if (backtrack(row, col, word, 0))
-          return true;
+        if (backtrack(row, col, word, 0)) {
+        // if (backtrack2(row, col, word, 0)) {
+            return true;
+        }
     return false;
     
     function backtrack(row, col,  word, index) {
@@ -30,5 +32,28 @@
         /* Step 4). clean up and return the result. */
         board[row][col] = word.charAt(index);
         return false;
+      }
+      function backtrack2( row,  col,  word,  index) {
+        /* Step 1). check the bottom case. */
+        if (index >= word.length)
+          return true;
+    
+        /* Step 2). Check the boundaries. */
+        if (row < 0 || row === ROWS || col < 0 || col === COLS
+            || board[row][col] != word.charAt(index))
+          return false;
+    
+        /* Step 3). explore the neighbors in DFS */
+        let ret = false;
+        // mark the path before the next exploration
+        board[row][col] = '#';
+    
+        for ( let [r,c] of [[1,0],[-1,0],[0,1],[0-1]]){
+            if (backtrack(row + r, col + c, word, index + 1)) break;
+        }
+    
+        /* Step 4). clean up and return the result. */
+        board[row][col] = word.charAt(index);
+        return ret;
       }
   }
