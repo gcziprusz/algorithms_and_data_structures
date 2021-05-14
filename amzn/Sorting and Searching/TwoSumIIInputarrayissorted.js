073d1f007398404139c8fb9039867e9a3faaaa53
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-05-14 17:15:25 -0500

    Create TwoSumIIInputarrayissorted.js

---
+++ amzn/Sorting and Searching/TwoSumIIInputarrayissorted.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let l = 0,r = numbers.length-1;
+    while(l<r){
+          if(numbers[l]+numbers[r] > target){
+              r -= 1;
+          }
+          else if(numbers[l]+numbers[r] < target){
+              l+=1;
+          } else {
+              return [l+1,r+1];
+          }
+    } 
+    return [];
+};
