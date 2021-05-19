0b72e70e5973109c1ae6bb3bf6afe9d033bb6234
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-05-19 06:55:14 -0500

    Create KClosestPointstoOrigin.js

---
+++ amzn/Sorting and Searching/KClosestPointstoOrigin.js
@@ -0,0 +1,9 @@
+/** SORTING SOLUTION
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+    points.sort((a,b)=> (Math.pow(a[0],2)+Math.pow(a[1],2)) - (Math.pow(b[0],2)+Math.pow(b[1],2)));
+    return points.slice(0,k);
+};
