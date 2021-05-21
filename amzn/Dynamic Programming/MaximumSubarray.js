d9b379b917533274e46eb85cfa88ac610ab07cbf
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-05-20 21:14:18 -0500

    Update MaximumSubarray.js

--- amzn/Dynamic Programming/MaximumSubarray.js
+++ amzn/Dynamic Programming/MaximumSubarray.js
@@ -2,6 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-    
-};
+function maxSubArray(A) {
+  var prev = 0;
+  var max = -Number.MAX_VALUE;
+
+  for (var i = 0; i < A.length; i++) {
+    prev = Math.max(prev + A[i], A[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
+}
