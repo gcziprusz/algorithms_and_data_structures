6ae44d472b42885afed0457f7e431243eae6966e
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-05-23 15:16:42 -0500

    Create event_emitter.js

---
+++ event_emitter.js
@@ -0,0 +1,104 @@
+// PROTOTYPE
+/* Polyfill indexOf. */
+var indexOf;
+
+if (typeof Array.prototype.indexOf === 'function') {
+    indexOf = function (haystack, needle) {
+        return haystack.indexOf(needle);
+    };
+} else {
+    indexOf = function (haystack, needle) {
+        var i = 0, length = haystack.length, idx = -1, found = false;
+
+        while (i < length && !found) {
+            if (haystack[i] === needle) {
+                idx = i;
+                found = true;
+            }
+
+            i++;
+        }
+
+        return idx;
+    };
+};
+
+
+/* Polyfill EventEmitter. */
+var EventEmitter = function () {
+    this.events = {};
+};
+
+EventEmitter.prototype.on = function (event, listener) {
+    if (typeof this.events[event] !== 'object') {
+        this.events[event] = [];
+    }
+
+    this.events[event].push(listener);
+};
+
+EventEmitter.prototype.removeListener = function (event, listener) {
+    var idx;
+
+    if (typeof this.events[event] === 'object') {
+        idx = indexOf(this.events[event], listener);
+
+        if (idx > -1) {
+            this.events[event].splice(idx, 1);
+        }
+    }
+};
+
+EventEmitter.prototype.emit = function (event) {
+    var i, listeners, length, args = [].slice.call(arguments, 1);
+
+    if (typeof this.events[event] === 'object') {
+        listeners = this.events[event].slice();
+        length = listeners.length;
+
+        for (i = 0; i < length; i++) {
+            listeners[i].apply(this, args);
+        }
+    }
+};
+
+EventEmitter.prototype.once = function (event, listener) {
+    this.on(event, function g () {
+        this.removeListener(event, g);
+        listener.apply(this, arguments);
+    });
+};
+
+// ES6 CLASSES
+
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+  on(event, listener) {
+      if (typeof this.events[event] !== 'object') {
+          this.events[event] = [];
+      }
+      this.events[event].push(listener);
+      return () => this.removeListener(event, listener);
+  }
+  removeListener(event, listener) {
+    if (typeof this.events[event] === 'object') {
+        const idx = this.events[event].indexOf(listener);
+        if (idx > -1) {
+          this.events[event].splice(idx, 1);
+        }
+    }
+  }
+  emit(event, ...args) {
+    if (typeof this.events[event] === 'object') {
+      this.events[event].forEach(listener => listener.apply(this, args));
+    }
+  }
+  once(event, listener) {
+    const remove = this.on(event, (...args) => {
+        remove();
+        listener.apply(this, args);
+    });
+  }
+};
