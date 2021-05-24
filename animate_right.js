cb13b6b2f1ab20fbfda51fd8a92f4837d2adfa0e
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-05-24 07:23:53 -0500

    Update animate_right.js

--- animate_right.js
+++ animate_right.js
@@ -1,5 +1,46 @@
+/*This week, we’ll be solving a coding problem that was given in an actual Facebook phone screen interview. Please
+comment below with your solutions! And be sure 
+to look at the Facebook Interview Questions course on Coderbyte for more Facebook related challenges!
 
+Write a function that takes a DOM element and smoothly animates it from its current position to distance pixels 
+to the right over duration milliseconds. Implement the following function, animate(el, milliseconds, distance)
 
+For example, animate(document.querySelector('#myDiv'), 2000, 100) would move the element with id myDiv 100px to 
+the right over 2 seconds. Implement this function without using jQuery or any other third-party libraries.*/
+
+// var el = document.getElementById("myDiv");
+
+// function animate(el, totalTime, distance, position) {
+//     if (position >= distance) return;
+//     startTime = performance.now();
+
+//     var expired = (new Date() - startTime) / 1000;
+//     position = expired * distance / totalTime * 1000; 
+
+//     el.style.left = position + 'px';
+
+//     requestAnimationFrame(function() {
+//         animate(el, totalTime, distance, position);
+//     });
+// }
+
+// animate(el, 2000, 300, 50);
+
+function animate(el,duration,distance){
+  let start = performance.now();
+  let speed = distance / duration;
+
+  function move(ctime){
+     // figure out timeElapsed , break out
+    let elapsed = ctime - start;
+    if (elapsed > duration) return;
+    el.style.transform = `translateX(${elapsed*speed}px)`;
+    window.requestAnimationFrame(move);
+  }
+  window.requestAnimationFrame(move);
+}
+
+animate(document.getElementById("mydiv"), 2000, 100);
 // HTML
 // <!DOCTYPE html>
 // <html>
