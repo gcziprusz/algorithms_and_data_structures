0f396389353b974c79467c3e40570c4b69521334
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-05-24 20:58:32 -0500

    Update dom_store.js

--- dom_store.js
+++ dom_store.js
@@ -59,3 +59,41 @@ resetButton.addEventListener('click', function() {
   store.remove(countButton);
   countButton.innerText = 0;
 });
+
+
+/*alternative using Map*/
+
+
+class Store {
+  constructor(){
+    this.nodes = new Map();
+  }
+  
+  has(node) {
+    return this.nodes.has(node);
+  }
+  set(node,value){
+    this.nodes.set(node,value);
+  }
+  get(node){
+      return this.nodes.get(node);
+  }
+}
+
+const node1 = document.createElement("div");
+const node2 = document.createElement("a");
+const node3 = document.createElement("span");
+const node4 = document.createElement("ul");
+
+const store = new Store();
+
+store.set(node1,1);
+store.set(node2,2);
+store.set(node3,3);
+
+console.assert(store.get(node1) === 1) // 1
+console.assert(store.get(node2) === 2) // 2
+console.assert(store.get(node3) === 3)  // 3
+
+console.assert(store.has(node1) ===true) // true
+console.assert(store.has(node4) ===false) // false
