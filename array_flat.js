51a2b6e3623785e9b11f275cad5fcaf8422489ef
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-05-27 07:14:57 -0500

    Update array_flat.js

--- array_flat.js
+++ array_flat.js
@@ -19,3 +19,25 @@ console.log(JSON.stringify([1,2,3]) === JSON.stringify(flatten([[1],2,3])));
 console.log(JSON.stringify([1,2,3,4])=== JSON.stringify(flatten([[1],2,3,[[[4]]]])));
 console.log(JSON.stringify([])=== JSON.stringify(flatten([[[[]]]])));
 
+
+
+/**functional alternative/
+/**
+ * [1,2,[3,4]] -> [1,2,3,4]
+ */
+
+let arr = [1,2,[3,4, [5,6, [7, [8, 9, 10]]]]]
+
+function flatten(arr) {
+  return arr.reduce(function(acc, next){
+    let isArray =  Array.isArray(next)
+    return acc.concat(isArray ? flatten(next) : next)
+  }, [])
+}
+
+if (!Array.prototype.flatten) {
+  Array.prototype.flatten = function() {
+    return flatten(this)
+  }
+}
+console.log(arr.flatten());
