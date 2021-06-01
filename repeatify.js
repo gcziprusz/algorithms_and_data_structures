589a367ea66257404be8ea803fce4ab579485f7f
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-01 06:33:48 -0500

    Create repeatify.js

---
+++ repeatify.js
@@ -0,0 +1,10 @@
+String.prototype.repeatify = String.prototype.repeatify  || function(times) {
+   var str = '';
+
+   for (var i = 0; i < times; i++) {
+      str += this;
+   }
+
+   return str;
+};
+console.log('hellbo'.repeatify(3));
