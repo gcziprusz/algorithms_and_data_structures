bf48a39c50b194506f2c5b430aca3e479edca2c2
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-04 12:11:18 -0500

    Update CSS&DOMHandClock.js

--- CSS&DOMHandClock.js
+++ CSS&DOMHandClock.js
@@ -1,3 +1,4 @@
+// DEMO https://codepen.io/gcziprusz/pen/dyvmBpL
 // CSS & DOM Hand Clock
 // Implement a live hand-clock using only HTML, CSS and JavaScript. If time allows, make sure the various clock hands are easily distinguishable.
 
