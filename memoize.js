bbe8cf9ad48f715bb52179cb8ad7419a4b917352
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-04 23:18:56 -0500

    Create memoize.js

---
+++ memoize.js
@@ -0,0 +1,51 @@
+
+function memoize(func) {
+  var memo = {};
+  var slice = Array.prototype.slice;
+
+  return function() {
+    var args = slice.call(arguments);
+
+    if (args in memo)
+      return memo[args];
+    else
+      return (memo[args] = func.apply(this, args));
+
+  }
+}
+
+
+/*** ALTERNATIVE ***/
+const memoize = (func) => {
+  const results = {};
+  return (...args) => {
+    const argsKey = JSON.stringify(args);
+    if (!results[argsKey]) {
+      results[argsKey] = func(...args);
+    }
+    return results[argsKey];
+  };
+};
+
+const clumsysquare = memoize((num) => {
+  let result = 0;
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= num; j++) {
+      result++;
+    }
+  }
+  return result;
+});
+
+console.time("First call");
+console.log(clumsysquare(9467));
+console.timeEnd("First call");
+
+// use the same value two times
+console.time("Second call");
+console.log(clumsysquare(9467));
+console.timeEnd("Second call");
+
+console.time("Third call");
+console.log(clumsysquare(9467));
+console.timeEnd("Third call");
