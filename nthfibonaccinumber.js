5d9be63e378e752a46af01e4145ff579a3f4b38f
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-06 15:04:39 -0500

    Update nthfibonaccinumber.js

--- nthfibonaccinumber.js
+++ nthfibonaccinumber.js
@@ -6,6 +6,14 @@ const fibonacci = (n) => {
   return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
+/*alternative*/
+function fibonacci(n) {
+   return n < 1 ? 0
+        : n <= 2 ? 1
+        : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
 
 
 var fibo = function(computed, number) {
@@ -22,3 +30,5 @@ if (number < 3) {
  return computed[number — 1] + computed[number — 2];
  }
 }
+
+
