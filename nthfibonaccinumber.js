acaa1d0f3eb413fc8fc5cf6d884bf4b4128ce29a
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-06 15:11:13 -0500

    Update nthfibonaccinumber.js

--- nthfibonaccinumber.js
+++ nthfibonaccinumber.js
@@ -15,21 +15,25 @@ function fibonacci(n) {
 }
 
 
-
-
-var fibo = function(computed, number) {
-if (number < 3) {
- return 1;
- }
- // If fibonacci for this this position is calculated just return.
- // This is called memoization (Dynamic Programming)
- if (computed[number]) {
- return computed[number]
- } else {
- computed[number — 1] = fibo(computed, number — 1);
- computed[number — 2] = fibo(computed, number — 2);
- return computed[number — 1] + computed[number — 2];
- }
+function fibonacci(n) {
+   return n < 1 ? 0
+        : n <= 2 ? 1
+        : memoizedFib(n - 1) + memoizedFib(n - 2);
 }
+
+let memoizedFib = memoize(fibonacci);
+console.log(memoizedFib(10));
+
+function memoize(fn){
+  let mem = new Map();
+  return function(...args){
+    let k = JSON.stringify(args)
+    if(mem.has(k)) return mem.get(k);
+    let res = fn.apply(this,args);
+    mem.set(k,res);
+    return res;
+  }
+}
+
 
 
