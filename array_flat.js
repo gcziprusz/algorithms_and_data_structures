e4a1f87ebcf3047ea1a191d7bd2f2c497558b1cd
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-12 21:57:30 -0500

    Update array_flat.js

--- array_flat.js
+++ array_flat.js
@@ -41,3 +41,21 @@ if (!Array.prototype.flatten) {
   }
 }
 console.log(arr.flatten());
+
+/**ITERATIVE */
+function flatten(arr) {
+  var array = [];
+  while(arr.length) {
+    var value = arr.shift();
+    if(Array.isArray(value)) {
+      // this line preserve the order
+      arr = value.concat(arr);
+    } else {
+      array.push(value);
+    }
+  }
+  return array;
+}
+
+flatten([1,[2,[3]],[4]]); // => [1,2,3,4]
+
