782815c4617e1f1d845ad7cc3c00ba3a0abb16ff
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-13 12:38:42 -0500

    Update throttle.js

--- throttle.js
+++ throttle.js
@@ -11,6 +11,27 @@ const throttle = (callback, interval) => {
   }
 }
 
+// Throttle with stasing arguments and using them if no new call arrives
+const throttle = (callback, interval) => {
+  let whitelisted = true;
+  let argStash=null;
+  return (...args) => {
+    if (!whitelisted) {
+      argStash = [...args];
+      return;
+    };
+    callback.apply(this, args);
+    whitelisted = false;
+    setTimeout( () => {
+      whitelisted = true;
+      argStash && callback.apply(this, argStash);
+      argStash= null;
+    },interval)
+  }
+}
+
+
+
 function log(m){console.log("mM",m)}
 let throttledLog = throttle(log,100);
 throttledLog("1");
