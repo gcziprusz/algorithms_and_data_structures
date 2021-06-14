c488990e517b5c592e013a0f05fb6bca8b6c9273
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-14 06:44:39 -0500

    Update queue_by_stack.js

--- queue_by_stack.js
+++ queue_by_stack.js
@@ -1,46 +1,50 @@
+
+// QUEUE FIFO
+// -------------------
+//  #4>  #3>  #2> #1>
+// -------------------
+//  ^               ^
+//  |               |
+//enqueue          peek
+//                dequeue
+
+
+// STACK FILO
+//   ??
+// | #4 |    |#1|
+// | #3 |    |#2| 
+// | #2 |    |#3|      
+// | #1 |    |#4|
+// _______  ______
+
 class Queue {
-  constructor() {
-    this.pushStack = new Stack()
-    this.popStack = new Stack()
+  constructor(){
+    this.pushStack = new Stack();
+    this.popStack = new Stack();
   }
-  
-  enqueue(element) { 
-    this.pushStack.push(element)  
+  enqueue(element) { /* add element to queue, similar to Array.prototype.push */ 
+    this.pushStack.push(element);
   }
-  
-  _move() {
-     while (this.pushStack.size() > 0) {
-       this.popStack.push(this.pushStack.pop())
-     }
-  }
-  
-  peek() { 
-    if (this.popStack.size() > 0) {
-      return this.popStack.peek()
+  peek() { /* get the head element*/ 
+    if(this.popStack.size() >0){
+      this.popStack.peek();
     }
-    
-    if (this.pushStack.size() > 0) {
-      this._move()
-      return this.popStack.peek()
-    }
-    
-    return undefined
+    this._pour()
+    return this.popStack.peek()
   }
-  
-  size() { 
-    return this.pushStack.size() + this.popStack.size()
+  _pour(){
+     while(this.pushStack.size()>0){
+      this.popStack.push(this.pushStack.pop())
+    }
   }
-  
-  dequeue() {
-    if (this.popStack.size() > 0) {
-      return this.popStack.pop()
+  dequeue() { /* remove the head element, similar to Array.prototype.pop */ 
+     if(this.popStack.size() >0){
+      this.popStack.peek();
     }
-    
-    if (this.pushStack.size() > 0) {
-      this._move()
-      return this.popStack.pop()
-    }
-    
-    return undefined
+    this._pour()
+    return this.popStack.pop()
+  }
+  size() { /* count of elements */ 
+    return this.pushStack.size()+this.popStack.size();
   }
 }
