5cb5f4ee8b23f13d70777bed6107dbda1a241474
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-14 21:51:20 -0500

    Update searching_symetric_node.js

--- searching_symetric_node.js
+++ searching_symetric_node.js
@@ -84,3 +84,25 @@ console.log(nodeX === node2); // true
 
 // [level_3_index,level_2_index,level_1_index,level_0_index];
 
+
+
+/**
+ * Using the node walker API 
+ * @param {HTMLElement} rootA
+ * @param {HTMLElement} rootB - rootA and rootB are clone of each other
+ * @param {HTMLElement} nodeA
+ */
+const findCorrespondingNode = (rootA, rootB, target) => {
+  const rootAWalker = document.createTreeWalker(rootA, NodeFilter.SHOW_ELEMENT);
+  const rootBWalker = document.createTreeWalker(rootB, NodeFilter.SHOW_ELEMENT);
+
+  let currentNodes = [rootAWalker.currentNode, rootBWalker.currentNode];
+
+  while (currentNodes[0] !== target) {
+    currentNodes = [rootAWalker.nextNode(), rootBWalker.nextNode()];
+  }
+
+  return currentNodes[1];
+}
+
+
