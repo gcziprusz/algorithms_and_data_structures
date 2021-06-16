2b3dc9e45398109b41769863294768b11d7a1ada
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-16 17:27:42 -0500

    Create reorder_array_new_indexes.js

---
+++ reorder_array_new_indexes.js
@@ -0,0 +1,12 @@
+function sort(items, newOrder) {
+   // [ 'F', 'A',  'E', 'D', 'C', 'B']
+   // keep swaping, until all elements are updated
+   
+   for (let i = 0; i < newOrder.length; i++) {
+     while (newOrder[i] !== i) {
+       const to = newOrder[i]
+       ;[newOrder[i], newOrder[to]] = [newOrder[to], newOrder[i]]
+       ;[items[i], items[to]] = [items[to], items[i]]
+     }
+   }
+}
