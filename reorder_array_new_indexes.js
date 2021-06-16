b3178a966ead195ea8a5721c6c39dcd1e40693bd
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-16 17:31:33 -0500

    Update reorder_array_new_indexes.js

--- reorder_array_new_indexes.js
+++ reorder_array_new_indexes.js
@@ -4,9 +4,9 @@ function sort(items, newOrder) {
    
    for (let i = 0; i < newOrder.length; i++) {
      while (newOrder[i] !== i) {
-       const to = newOrder[i]
-       ;[newOrder[i], newOrder[to]] = [newOrder[to], newOrder[i]]
-       ;[items[i], items[to]] = [items[to], items[i]]
+       const to = newOrder[i];
+       [newOrder[i], newOrder[to]] = [newOrder[to], newOrder[i]];
+       [items[i], items[to]] = [items[to], items[i]];
      }
    }
 }
