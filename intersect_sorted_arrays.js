e59e47b883f69e0e8e0dd9f9d547a5629926611f
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-17 18:09:26 -0500

    Update intersect_sorted_arrays.js

--- intersect_sorted_arrays.js
+++ intersect_sorted_arrays.js
@@ -20,3 +20,28 @@ function intersect(arr1, arr2) {
 
   return result
 }
+
+/*similar approach with reverse and pop */
+function intersect(arr1, arr2) {
+  arr1.reverse()
+  arr2.reverse()
+
+  const result = []
+
+  while (arr1.length > 0 && arr2.length > 0) {
+    const top1 = arr1[arr1.length - 1]
+    const top2 = arr2[arr2.length - 1]
+
+    if (top1 === top2) {
+      result.push(top1)
+      arr1.pop()
+      arr2.pop()
+    } else if (top1 < top2) {
+      arr1.pop()
+    } else {
+      arr2.pop()
+    }
+  }
+
+  return result
+}
