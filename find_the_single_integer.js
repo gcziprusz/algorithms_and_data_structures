b5c37970b62ef84d3c7e427e27ade31a1bb0996a
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-17 07:13:18 -0500

    Update find_the_single_integer.js

--- find_the_single_integer.js
+++ find_the_single_integer.js
@@ -22,3 +22,15 @@ function findSingle(arr) {
   return single.values().next().value;
 }
 
+// with a map very similar
+function findSingle(arr) {
+  let dictionary = new Map();
+
+  while (arr.length) {
+    character = arr.shift();
+    if (dictionary.has(character)) dictionary.delete(character);
+    else dictionary.set(character, true);
+  }
+
+  return dictionary.keys().next().value;
+}
