541a9cfc310703d3ebc7076d7913f689f8e4b559
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-17 20:59:11 -0500

    Update first_dupl_char_string.js

--- first_dupl_char_string.js
+++ first_dupl_char_string.js
@@ -8,3 +8,18 @@ function firstDuplicate(str) {
   }
   return null;
 }
+
+
+function firstDuplicate(str) {
+  // your code here
+
+  let duplicate = '';
+  for (var i = 0; i < str.length; i++) {
+    if (i !== str.lastIndexOf(str[i])) {
+      duplicate = str[i];
+      break;
+    }
+  }
+
+  return duplicate.length > 0 ? duplicate : null
+}
