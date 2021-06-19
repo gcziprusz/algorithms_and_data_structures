4d6533a42ab5698e0ddf262bcc11a673ff4c5869
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-19 12:57:27 -0500

    Update previous_left_sibling.js

--- previous_left_sibling.js
+++ previous_left_sibling.js
@@ -1,11 +1,9 @@
 function previousLeftSibling(root, target) {
-  let nodes = [root];
-  while (nodes.length) {
-    let node = nodes.pop();
-    if (node === target) {
-      return nodes.pop() || null;
-    }
-    nodes.unshift(...node.children);
+  let q = [root];
+  while(q.length){
+    let last = q.pop();
+    if(last === target) return q.pop() || null;
+    q.unshift(...last.children)
   }
   return null;
 }
