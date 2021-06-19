427a72dbf0d78a4bf979dcc5ac91f466ab84601a
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-19 12:53:33 -0500

    Update previous_left_sibling.js

--- previous_left_sibling.js
+++ previous_left_sibling.js
@@ -1,14 +1,11 @@
-// BFS
 function previousLeftSibling(root, target) {
-  const queue = [root];
-  while (queue.length) {
-    let size = queue.length;
-    while (size > 0) {
-      const pop = queue.pop();
-      if (pop === target) return queue.length === 0 ? null : queue.pop();
-      queue.unshift(...pop.children);
-      size -= 1;
+  let nodes = [root];
+  while (nodes.length) {
+    let node = nodes.pop();
+    if (node === target) {
+      return nodes.pop() || null;
     }
+    nodes.unshift(...node.children);
   }
   return null;
 }
