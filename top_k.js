6db9eb8b33ec2203859b3c5f455dcc00bf57ce07
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-19 17:20:48 -0500

    Update top_k.js

--- top_k.js
+++ top_k.js
@@ -3,3 +3,18 @@
 function topK(arr: number[], k: number): number[] {
   return arr.sort((a, b) => a > b ? -1 : 1).slice(0, k);
 }
+
+
+import Heap from 'heap-js';
+function topK(arr, k) {
+  const minHeap = new Heap();
+  arr.forEach(value => {
+    minHeap.push(value) // O(logk)
+
+    if (minHeap.length > k) {
+      minHeap.pop()
+    }  
+  })
+
+  return minHeap.toArray()
+}
