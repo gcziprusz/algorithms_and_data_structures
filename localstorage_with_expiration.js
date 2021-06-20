864a9234ac863e84a8c6d7981e6236f044ab8b67
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-20 08:25:03 -0500

    Update localstorage_with_expiration.js

--- localstorage_with_expiration.js
+++ localstorage_with_expiration.js
@@ -1,3 +1,4 @@
+/*QUICK DIRTY RACY*/
 window.myLocalStorage = {
   getItem(key) {
     return window.localStorage.getItem(key)
@@ -16,5 +17,41 @@ window.myLocalStorage = {
   },
   clear() {
     window.localStorage.clear()
+  }
+}
+
+/*BETTER WAY */
+window.myLocalStorage = {
+  getItem(key) {
+    try {
+      const {value, maxAge, createdAt} = JSON.parse(localStorage.getItem(key))
+      
+      if (maxAge !== undefined && Date.now() - createdAt >= maxAge) {
+        this.removeItem(key)
+        return null
+      }
+      
+      return value
+    } catch (e) {
+      return null
+    }    
+  },
+  
+  setItem(key, value, maxAge) {
+    const entry = {
+      value,
+      maxAge,
+      createdAt: Date.now()
+    }
+    
+    localStorage.setItem(key, JSON.stringify(entry))
+  },
+  
+  removeItem(key) {
+    return localStorage.removeItem(key)
+  },
+  
+  clear() {
+    localStorage.clear()
   }
 }
