e14c1ec1adbc81cf115668a54c1761e79edb49e6
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-24 07:57:09 -0500

    Create jest_spy_on.js

---
+++ jest_spy_on.js
@@ -0,0 +1,18 @@
+function spyOn(obj, methodName) {
+  const calls = []
+
+  const originMethod = obj[methodName]
+  
+  if (typeof originMethod !== 'function') {
+    throw new Error('not function')
+  }
+  
+  obj[methodName] = function(...args) {
+    calls.push(args)
+    return originMethod.apply(this, args)
+  }
+  
+  return {
+    calls
+  }
+}
