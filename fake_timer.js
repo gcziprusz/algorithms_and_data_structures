f8c7a4483ac67ee8be0257b30cc0f46af10c6700
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-24 07:49:04 -0500

    Update fake_timer.js

--- fake_timer.js
+++ fake_timer.js
@@ -1,51 +1,42 @@
 class FakeTimer {
   install() {
-    // replace window.setTimeout, window.clearTimeout, Date.now with your implementation
-    this.windowSetTimeout = window.setTimeout
-    this.windowClearTimeout = window.clearTimeout
-    this.dateNow = Date.now
-    this.callbacks = []
-    this.currentTime = 0
-    this.id = 1
-    window.setTimeout = (callback, ms = 0) => {
-      const wait = this.currentTime + ms
-      // find index to insert to
-      // index of a callback indicates the order that it will be executed
-      const numPrevCallbacks = this.callbacks.reduce((count, item) => {
+    this.originalsetTimeout = window.setTimeout;
+    this.originalclearTimeout = window.clearTimeout;
+    this.originalDateNow = Date.now;
+    this.currentTime = 0;
+    this.callbacks = [];
+    window.setTimeout = (callback,ms) => {
+       let wait = this.currentTime +ms;
+       const insertAt = this.callbacks.reduce((count, item) => {
         return item.wait <= wait ? count + 1 : count
       }, 0)
-      // insert callback item
-      const id = this.id
-      const item = { callback, wait, id }
-      this.callbacks.splice(numPrevCallbacks, 0, item)
-      // increment id for next item
-      this.id += 1
-      return id
+      let id = Symbol()
+      let item = {id, wait, callback};
+      this.callbacks.splice(insertAt,0,item);
+      return id;
     }
     window.clearTimeout = (id) => {
       const index = this.callbacks.findIndex((item) => {
         return item.id === id
       })
       if (index > -1) {
-        const { wait } = this.callbacks[index]
-        if (wait < this.currentTime) return
-        this.callbacks.splice(index, 1)
+        const { wait } = this.callbacks[index];
+        if (wait < this.currentTime) return;
+        this.callbacks.splice(index, 1);
       }
-    }
-    Date.now = () => {
-      return this.currentTime
-    }
+    } 
+    Date.now = () => this.currentTime;
   }
+
   uninstall() {
-    // restore the original implementation of window.setTimeout, window.clearTimeout, Date.now
-    window.setTimeout = this.windowSetTimeout
-    window.clearTimeout = this.windowClearTimeout
-    Date.now = this.dateNow
+    window.setTimeout = this.originalsetTimeout ;
+    window.clearTimeout = this.originalclearTimeout ;
+    Date.now = this.originalDateNow;
   }
+
   tick() {
-    // execute all callbacks in this.callbacks, from the first to the last item
-    while (this.callbacks.length > 0) {
-      const { callback, wait } = this.callbacks.shift();
+    while(this.callbacks.length) {
+      let {wait,callback} = this.callbacks.shift();
       this.currentTime = wait;
       callback();
     }
