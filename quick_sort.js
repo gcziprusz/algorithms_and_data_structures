b62ba667a422a1d4e2330d4c5ef29e7e409545df
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-27 10:53:13 -0500

    Create quick_sort.js

---
+++ quick_sort.js
@@ -0,0 +1,20 @@
+function quickSort(arr, l=0, r=arr.length-1) {
+  if (l>r) return;
+  const pIndex=partition(arr,l,r);
+  quickSort(arr,l,pIndex-1);
+  quickSort(arr,pIndex+1,r);
+}
+
+function partition(arr,l,r) {
+  const pivot=l++;
+  while (l<=r) {
+    if (arr[l]<=arr[pivot])
+      l++;
+    else {
+      [arr[l], arr[r]]=[arr[r], arr[l]];
+      r--;
+    }
+  }
+  [arr[pivot], arr[r]]=[arr[r], arr[pivot]];
+  return r;
+}
