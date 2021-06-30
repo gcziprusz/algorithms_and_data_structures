3a49de6a158231492780fffa91d16fbe2ed12f06
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-06-29 20:45:23 -0500

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -9,3 +9,17 @@ Array.prototype.myReduce = function (...args) {
   }
   return result;
 }
+
+
+
+//reduce
+Array.prototype.reduce = function(func, initState) {
+  const arr = this
+  const callback = func
+  let init = initState
+
+  arr.forEach(function(value, index){
+      init=callback(init, value)
+  })
+  return init
+}
