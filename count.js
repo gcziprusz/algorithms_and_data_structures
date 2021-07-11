f4eeff799f496c98055d8c4908a1adc6106db07a
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-07-11 16:26:39 -0500

    Update count.js

--- count.js
+++ count.js
@@ -4,3 +4,13 @@ const count = (() =>{
   fn.reset = () => n=0;
   return fn;
 })();
+
+
+// your code here
+function count(){
+  count.times = (count.times ||0) +1
+  return count.times;
+}
+count.reset = function() {
+  count.times = 0;
+}
