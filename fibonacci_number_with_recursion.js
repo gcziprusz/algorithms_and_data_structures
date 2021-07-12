167845cbe0cdc2f1d6fca98ae7d5055c0adfe0c1
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-07-12 08:10:00 -0500

    Update fibonacci_number_with_recursion.js

--- fibonacci_number_with_recursion.js
+++ fibonacci_number_with_recursion.js
@@ -1,3 +1,10 @@
+// index/input> 
+// 0 1 2 3 4 5 6 7  8  9  10
+// | | | | | | | |  |  |  |
+// v v v v v v v v  v  v  v    
+// 0 1 1 2 3 5 8 13 21 34 55
+// fib sequence
+
 // simple exponential 
    function fib(n) {
     if (n <= 1)
