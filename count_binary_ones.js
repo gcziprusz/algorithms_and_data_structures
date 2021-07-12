93b22698bf628094749e280bc6d4a4ed6b4e9c46
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-07-12 08:00:59 -0500

    Update count_binary_ones.js

--- count_binary_ones.js
+++ count_binary_ones.js
@@ -24,3 +24,13 @@ function countOne(n)
     }
     return count;
 }
+// ALTERB+NATIVE USING SHIFTING
+function countOne(n)
+{
+  let count =0;
+  while (n){
+    count += n &1;
+    n>>=1;
+  }
+  return count
+}
