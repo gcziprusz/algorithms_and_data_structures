56cb4c910fb4c3ae63e0fcd22c8d02d739903cbb
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-07-12 20:56:02 -0500

    Update event_delegation.js

--- event_delegation.js
+++ event_delegation.js
@@ -1,9 +1,69 @@
+// Map<node, Array<[predicate, hanlder]>>
+const allHandlers = new Map()
+
+/**
+ * @param {HTMLElement} root
+ * @param {(el: HTMLElement) => boolean} predicate
+ * @param {(e: Event) => void} handler
+ */
 function onClick(root, predicate, handler) {
-  // your code here
-  if(predicate(root)){
-      root.addEventListener('click', handler);
+  if (allHandlers.has(root)) {
+    allHandlers.get(root).push([predicate, handler])
+    return
   }
-  root.childNodes?.forEach( node => {
-    onClick(node,predicate,handler)
-  })
+
+  allHandlers.set(root, [[predicate, handler]])
+
+  // attach the real handler
+  root.addEventListener('click', function(e) {
+
+    // from e.target -> root
+    // check if element shoulded applied witht handler
+
+    let el = e.target
+    const handlers = allHandlers.get(root)
+    let isPropagationStopped = false
+
+    e.stopPropagation = () => {
+      isPropagationStopped = true
+    }
+
+    while (el) {
+      
+      let isImmediatePropagationStopped = false
+
+      e.stopImmediatePropagation = () => {
+        isImmediatePropagationStopped = true
+        isPropagationStopped = true
+      }
+
+      for (const [predicate, handler] of handlers) {
+        if (predicate(el)) {
+          handler.call(el, e)
+
+          // check immediatepropagtion
+          if (isImmediatePropagationStopped) {
+            break
+          }
+        }
+      }
+
+      // check propagation
+      if (el === root || isPropagationStopped) break
+
+      el = el.parentElement
+    }
+
+  }, false)
 }
+
+
+// function onClick(root, predicate, handler) {
+//   // your code here
+//   if(predicate(root)){
+//       root.addEventListener('click', handler);
+//   }
+//   root.childNodes?.forEach( node => {
+//     onClick(node,predicate,handler)
+//   })
+// }
