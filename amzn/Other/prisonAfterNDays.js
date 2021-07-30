deeb35d9376e5fabab6f98fca738cf367973b815
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-07-29 20:36:02 -0500

    Update prisonAfterNDays.js

--- amzn/Other/prisonAfterNDays.js
+++ amzn/Other/prisonAfterNDays.js
@@ -20,3 +20,31 @@ var prisonAfterNDays = function(cells, N) {
     }
     return prison.split('');
 };
+
+
+/*** EASIER TO GROK ***/
+function nextDay(prisonState){
+    let prisonOnNextDay = '';
+    for(let i =0 ; i< prisonState.length ; i++){
+        if(prisonState[i-1] === prisonState[i+1]) prisonOnNextDay +='1';
+        else prisonOnNextDay +='0';
+    }
+    return prisonOnNextDay;
+}
+function prisonAfterNDays(cells, N) {
+    let  seen = new Map(), cycle = false, cycleLen=0, p=cells.join("");
+    while(N > 0) {
+        if(!cycle){
+            if(seen.has(p)) {
+                cycleLen = seen.get(p)-N;
+                N%=cycleLen;
+                cycle=true;
+            } else seen.set(p,N)
+        }
+        if(N>0) {
+            N -=1;
+            p = nextDay(p);
+        }
+    }
+    return p.split("");
+}
