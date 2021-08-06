85c0c56ef1917d588274fcc55234b21dc606772f
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-08-05 21:26:08 -0500

    Update pairs_of_songs_with_total_durations_divisible_by60.js

--- amzn/pairs_of_songs_with_total_durations_divisible_by60.js
+++ amzn/pairs_of_songs_with_total_durations_divisible_by60.js
@@ -8,3 +8,19 @@ var numPairsDivisibleBy60 = function(time) {
     }
     return count;
 };
+
+
+
+function numPairsDivisibleBy60(time) {
+    let remainders = new Array(60).fill(0);
+    let count = 0;
+    for (let t of time) {
+        if (t % 60 == 0) { // check if a%60==0 && b%60==0
+            count += remainders[0];
+        } else { // check if a%60+b%60==60
+            count += remainders[60 - t % 60];
+        }
+        remainders[t % 60]++; // remember to update the remainders
+    }
+    return count;
+}
