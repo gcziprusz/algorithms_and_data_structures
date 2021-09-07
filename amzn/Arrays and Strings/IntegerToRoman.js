1135202992df196c09b7f4eba6add61f2a5bd78d
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-09-07 17:24:58 -0500

    Update IntegerToRoman.js

--- amzn/Arrays and Strings/IntegerToRoman.js
+++ amzn/Arrays and Strings/IntegerToRoman.js
@@ -8,3 +8,29 @@ var intToRoman = function(num) {
     }
     return roman;
 };
+
+let dict = new Map([
+  ['M',1000],
+  ['CM',900],
+  ['D',500],
+  ['CD',400],
+  ['C',100],
+  ['XC',90],
+  ['L',50],
+  ['XL',40],
+  ['X',10],
+  ['IX',9],
+  ['V',5],
+  ['IV',4],
+  ['I',1],
+]);
+function romanToInteger(num) {
+  let r='';
+  for(let [k,v] of dict){
+     while(num >= v){
+       r+= k;
+       num -=v;
+     }
+  }
+  return r;
+}
