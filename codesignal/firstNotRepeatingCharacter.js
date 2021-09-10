f915f28237ec7f42ef95358c72f1d8b7359a19a6
Gabriel Cziprusz <gcziprusz@gmail.com>
2021-09-10 06:55:59 -0500

    Update firstNotRepeatingCharacter.js

--- codesignal/firstNotRepeatingCharacter.js
+++ codesignal/firstNotRepeatingCharacter.js
@@ -1,10 +1,20 @@
 function firstNotRepeatingCharacter(s) {
-let m = new Map();
-for(let c of s){
-    m.set(c,(m.get(c)||0)+1)
+    let m = new Map();
+    for(let c of s){
+        m.set(c,(m.get(c)||0)+1)
+    }
+    for(let [char,occurence] of m){
+        if(occurence===1) return char;
+    }
+    return "_";
 }
-for(let [char,occurence] of m){
-    if(occurence===1) return char;
-}
-return "_";
+
+function firstNotRepeatingCharacter(s) {
+    var arr = s.split("");
+    for(var i = 0; i < arr.length; i++){
+        if( arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])){
+            return arr[i]
+        }
+    }
+    return "_"
 }
