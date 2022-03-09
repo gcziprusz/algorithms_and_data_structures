992b135536e799f8fd221ac38b6ba59d8332fcc3
Gabriel Cziprusz <gcziprusz@gmail.com>
2022-03-09 06:32:54 -0600

    Update one.js

--- CtCI/ch2/one.js
+++ CtCI/ch2/one.js
@@ -1,4 +1,31 @@
 // https://leetcode.com/problems/remove-duplicates-from-an-unsorted-linked-list/
 // 1836. Remove Duplicates From an Unsorted Linked List 
 
-// TBD
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicatesUnsorted = function(head) {
+  let histo = new Map();
+  let c = head;
+  while(c){
+      histo.set(c.val, (histo.get(c.val)||0)+1);
+      c = c.next;
+  }
+  let t = new ListNode(0,head);
+  c=t
+  while(c){
+      while(c.next && histo.get(c.next.val) >1){
+        c.next = c.next.next
+      }
+    c = c.next;
+  }
+  return t.next;  
+};
