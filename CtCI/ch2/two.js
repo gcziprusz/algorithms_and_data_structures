e17fed4320fd1891f469af985ad7b1be15714ff5
Gabriel Cziprusz <gcziprusz@gmail.com>
2022-04-06 05:59:59 -0500

    Update two.js

--- CtCI/ch2/two.js
+++ CtCI/ch2/two.js
@@ -13,6 +13,29 @@
  * @param {number} n
  * @return {ListNode}
  */
+
+// 2 pass algo to figure out length of list then substract N from it to calculate target node index 
+var removeNthFromEnd = function(head, n) {
+    let d = new ListNode(0);
+    d.next = head;
+    let len =0;
+    let first = head;
+    while (first) {
+        len++;
+        first = first.next;
+    }
+    let nth = len-n;
+    first = d;
+    
+    while(nth > 0) {
+        nth--;
+        first = first.next;
+    }
+    first.next = first.next.next;
+    return d.next;
+};
+
+// Single pass algo with 2 pointers that are kept N distance away from each other while scanning the list
 var removeNthFromEnd = function(head, n) {
     let d = new ListNode(0);
     d.next=head;
