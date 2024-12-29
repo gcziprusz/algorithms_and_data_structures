b04c13f1936a5329dc48dbee13db7a5e4636e537
Gabriel Cziprusz <gcziprusz3@gatech.edu>
2024-12-29 07:33:00 -0600

    238

---
+++ top150/238ProductofArrayExceptSelf.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/product-of-array-except-self/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        l = len(nums)
+        pref = [1] * l
+        suff = [1] * l
+    
+        for i in range(1,l):
+            pref[i] = nums[i-1]*pref[i-1]
+    
+        for i in range(l-2,-1,-1):
+            suff[i] = nums[i+1]*suff[i+1]
+
+        return [pref[i]*suff[i] for i in range(l)]
