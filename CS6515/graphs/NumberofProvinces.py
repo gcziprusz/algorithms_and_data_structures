9eab3db1a2a2b976ab5294e07d2b071589966ea6
Gabe Cziprusz <gcziprusz@gmail.com>
2025-05-05 07:24:24 -0500

    union find

--- CS6515/graphs/NumberofProvinces.py
+++ CS6515/graphs/NumberofProvinces.py
@@ -1,7 +1,7 @@
 # 547. Number of Provinces
 # https://leetcode.com/problems/number-of-provinces/editorial/?envType=study-plan-v2&envId=graph-theory
 
-# DFS
+# DFS & BFS
 class Solution(object):
     def dfs(self,node, isConnected,visited_cities):
         visited_cities[node] = True
@@ -36,7 +36,39 @@ class Solution(object):
 
         return province
 
-# BFS
-
 
 # Union-find
+class UnionFind:
+    def __init__(self,n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    def find(self,x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+        
+    def union(self,x,y):
+        xx = self.find(x)
+        yy = self.find(y)
+
+        if self.rank[xx] < self.rank[yy]:
+            self.parent[xx] = yy
+        elif self.rank[xx] > self.rank[yy]:
+            self.parent[yy] = xx
+        else:
+            self.parent[yy] = xx
+            self.rank[xx] = self.rank[xx] + 1
+
+        
+class Solution:
+    def findCircleNum(self, isConnected):
+        n = len(isConnected)
+        u = UnionFind(n)
+        provinces = n
+        for i in range(n):
+            for j in range(i + 1,n):
+                if isConnected[i][j] and u.find(i) != u.find(j):
+                    provinces = provinces - 1
+                    u.union(i,j)
+
+        return provinces
